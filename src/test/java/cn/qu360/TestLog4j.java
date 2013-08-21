bb6459f927efb6515d0412cbae35f537fa90d8bb
jiansheliu@gmail.com <jiansheliu@gmail.com>
2013-08-21 10:58:51 +0800

    add

---
+++ src/test/java/cn/qu360/TestLog4j.java
@@ -0,0 +1,18 @@
+package cn.qu360;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jianshe
+ * Date: 13-8-21
+ * Time: 上午10:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestLog4j {
+   Logger logger =  Logger.getLogger(TestLog4j.class);
+
+    public static void main(String[] args) {
+
+    }
+}
